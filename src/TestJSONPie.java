20f42904ba61159870deece3ac4e3d7572e7be4f
Shishir <anwarshahriar@gmail.com>
2014-11-12 23:23:02 -0800

    Updates demo main class

--- src/TestJSONPie.java
+++ src/TestJSONPie.java
@@ -9,42 +9,48 @@ public class TestJSONPie {
         JSONPie pie = null;
         try {
             pie = new JSONPie(json);
+
             String fullName = pie.getString("name->firstName") + " " + pie.getString("name->lastName");
             System.out.println(fullName);
+
+            for (int i = 0; i < pie.getJSONArray("contacts").length(); i++) {
+                String contact = pie.getString("contacts[" + i + "]");
+                System.out.println(contact);
+            }
+
+            for (int i = 0; i < pie.getJSONArray("doc").length(); i++) {
+                String title = pie.getString("doc[" + i + "] -> title");
+                String text = pie.getString("doc[" + i + "] -> text");
+                System.out.println(title + "\n" + text);
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
     private static String json = "{\n" +
-            "\t'name': {\n" +
-            "\t\t'id': 4,\n" +
-            "\t\t'firstName': 'Shahriar',\n" +
-            "\t\t'lastName': 'anwar'\n" +
-            "\t},\n" +
-            "\t'contacts': [\n" +
-            "\t\t'+8801686689608',\n" +
-            "\t\t'+8801681676824',\n" +
-            "\t\t'+8801552463683',\n" +
-            "\t\t'+880191760169'\n" +
-            "\t],\n" +
-            "\t'doc': [\n" +
-            "\t\t{\n" +
-            "\t\t\t'title': \"one\",\n" +
-            "\t\t\t'text': \"This is text for doc one.\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t'title': \"two\",\n" +
-            "\t\t\t'text': \"This is text for doc two.\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t'title': \"three\",\n" +
-            "\t\t\t'text': \"This is text for doc three.\"\n" +
-            "\t\t},\n" +
-            "\t\t{\n" +
-            "\t\t\t'title': \"four\",\n" +
-            "\t\t\t'text': \"This is text for doc four.\"\n" +
-            "\t\t}\n" +
-            "\t]\n" +
+            "  \"name\": {\n" +
+            "    \"firstName\": \"Shahriar\",\n" +
+            "    \"lastName\": \"Anwar\"\n" +
+            "  },\n" +
+            "  \"contacts\":[\n" +
+            "    \"1234\",\n" +
+            "    \"5678\",\n" +
+            "    \"9876\"\n" +
+            "  ],\n" +
+            "  \"doc\":[\n" +
+            "    {\n" +
+            "      \"title\": \"one\",\n" +
+            "      \"text\": \"this is doc one\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"title\": \"two\",\n" +
+            "      \"text\": \"this is doc two\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"title\": \"three\",\n" +
+            "      \"text\": \"this is doc three\"\n" +
+            "    }\n" +
+            "  ]\n" +
             "}";
 }
